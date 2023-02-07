67681ac437581b35fb4a9b44a6e97b4126199180
Victor Seleimend <vsennaselei@gmail.com>
2023-02-07 11:34:22 -0300

    inicialização

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+      <header className="bg-danger text-light py-5 text-center">
+        <h1 className="font-weight-bold">Buscador de Bebidas</h1>
+      </header>
+      <Container className='mt-5'>1</Container>
+    </>
+  );
+}
+
+export default App;
