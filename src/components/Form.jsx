cd655135a1505e557953239537b6dfdaf24d80de
Victor Seleimend <vsennaselei@gmail.com>
2023-02-07 12:03:43 -0300

    iterando nas categorias e exibindo as opções

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,9 @@
 import { Button, Form as Formulary, Row, Col } from 'react-bootstrap';
+import useCategories from '../hooks/useCategories';
 
 const Form = () => {
+  const { categories } = useCategories();
+
   return (
     <Formulary>
       <Row>
@@ -19,13 +22,24 @@ const Form = () => {
         </Col>
         <Col md={6}>
           <Formulary.Group className="mb-3">
-            <Formulary.Label htmlFor="category">
-              Categoria
-            </Formulary.Label>
-            <Formulary.Select id='category' name='category'>
+            <Formulary.Label htmlFor="category">Categoria</Formulary.Label>
+            <Formulary.Select id="category" name="category">
               <option>-- Selecionar Categoria --</option>
+              {categories.map(cat => (
+                <option key={cat.strCategory} value={cat.strCategory}>
+                  {cat.strCategory}
+                </option>
+              ))}
             </Formulary.Select>
           </Formulary.Group>
+        </Col>
+      </Row>
+      <Row className="justify-content-end">
+        <Col md={3}>
+          <Button
+            variant='danger'
+            className='text-uppercase w-100'
+          >Buscar Bebidas</Button>
         </Col>
       </Row>
     </Formulary>
