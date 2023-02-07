7d0dd6b0641acbf941b3e18719b1911c58fff396
Victor Seleimend <vsennaselei@gmail.com>
2023-02-07 11:54:45 -0300

    requisição para api nas categorias de drinks

--- src/context/CategoriesProvider.jsx
+++ src/context/CategoriesProvider.jsx
@@ -1,10 +1,33 @@
-import { createContext } from 'react';
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
 
 const CategoriesContext = createContext();
 
 const CategoriesProvider = ({ children }) => {
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    try {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+
+      const { data } = await axios(url);
+
+      setCategories(data.drinks);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
-    <CategoriesContext.Provider value={{}}>
+    <CategoriesContext.Provider
+      value={{
+        categories
+      }}
+    >
       {children}
     </CategoriesContext.Provider>
   );
